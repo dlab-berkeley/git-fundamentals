26b587ab01efc86db87f835e532e230558b8c752
Aaron Culich <aculich@gmail.com>
2018-12-09 17:15:23 -0800

    don't output anything when deleteing old rstudio container

--- rstudio-on-gcp.sh
+++ rstudio-on-gcp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -s -p 'Make up a throwaway password: ' PASS; echo; docker rm -f rstudio 2>/dev/null; docker run -v $(pwd):/home/rstudio --name rstudio -d -p 8080:8787 -e PASSWORD="$PASS" rocker/rstudio > /dev/null
+read -s -p 'Make up a throwaway password: ' PASS; echo; docker rm -f rstudio 2>&1 >/dev/null; docker run -v $(pwd):/home/rstudio --name rstudio -d -p 8080:8787 -e PASSWORD="$PASS" rocker/rstudio > /dev/null
 echo "Now click on the 'Web Preview' button above in Google Cloud Shell menubar and select 'Preview on Port 8080'."
 echo "A new browser tab will open and you can log in with the password you typed"
 echo "username: rstudio"
